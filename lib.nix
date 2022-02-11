525ca70397a1dd81eb36b6f23fb9d4b4831f0bbc
Akira Komamura <akira.komamura@gmail.com>
2022-02-11 18:54:27 +0900

    Ignore .dir-locals.el

--- lib.nix
+++ lib.nix
@@ -58,7 +58,8 @@ let
         if [[ "$el" = *-test.el ]] \
            || [[ "$el" = *-tests.el ]] \
            || [[ "$el" = test.el ]] \
-           || [[ "$el" = tests.el ]]
+           || [[ "$el" = tests.el ]] \\
+           || [[ "$el" = .dir-locals.el ]]
         then
           continue
         fi
