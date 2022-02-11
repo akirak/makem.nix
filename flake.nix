d928cf1d2675b77c45135783948e6698b096acd8
Akira Komamura <akira.komamura@gmail.com>
2022-02-11 18:32:29 +0900

    Get target systems from nix-emacs-ci

--- flake.nix
+++ flake.nix
@@ -32,37 +32,46 @@
     { self
     , nixpkgs
     , flake-utils
-      , ...
+    , ...
     } @ inputs:
-    flake-utils.lib.eachDefaultSystem
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    flake-utils.lib.eachSystem systems
       (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              (import (inputs.emacs-ci + "/overlay.nix"))
-              inputs.twist.overlay
-            ];
-          };
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (import (inputs.emacs-ci + "/overlay.nix"))
+            inputs.twist.overlay
+          ];
+        };
 
-          makeDrvs = pkgs.callPackage ./lib.nix {
-            emacs = pkgs.emacs-27-2;
-            lockDir = ./lock;
-            inventories = [
-              {
-                type = "elpa";
-                path = inputs.gnu-elpa.outPath + "/elpa-packages";
-              }
-              {
-                type = "melpa";
-                path = inputs.melpa.outPath + "/recipes";
-              }
-            ];
-            makem = inputs.makem.outPath;
-          };
+        makeDrvs = pkgs.callPackage ./lib.nix {
+          emacs = pkgs.emacs-27-2;
+          lockDir = ./lock;
+          inventories = [
+            {
+              type = "elpa";
+              path = inputs.gnu-elpa.outPath + "/elpa-packages";
+            }
+            {
+              type = "melpa";
+              path = inputs.melpa.outPath + "/recipes";
+            }
+          ];
+          makem = inputs.makem.outPath;
+        };
 
-          defaultDrvs = makeDrvs { };
-        in
+        defaultDrvs = makeDrvs { };
+      in
       rec {
         packages = flake-utils.lib.flattenTree {
           inherit (defaultDrvs.emacsForLint.admin "lock") lock update;
